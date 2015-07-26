71288ff316ab88a84017a55e9ab4d019071c5815
folse <folsee@gmail.com>
2015-07-26 11:00:39 +0800

    update settings

--- membership/settings.py
+++ membership/settings.py
@@ -106,11 +106,6 @@ DATABASES = {
 #     }
 # }
 
-
-
-# Internationalization
-# https://docs.djangoproject.com/en/1.8/topics/i18n/
-
 LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'Europe/Stockholm'
@@ -122,9 +117,6 @@ USE_L10N = False
 USE_TZ = False
 
 SESSION_COOKIE_AGE=60*60*24*365*100
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
 
